77897403653df1991b9d978f6aa49d2653ecb514
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-05-12 13:18:15 +0600

    add food post added

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json());
 
 console.log(process.env.DB_PASS)
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cgjyhyw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,14 +24,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // collection set up
+    const addFoodCollection =client.db('oaiFoodCorner').collection('food')
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // add food post
+    app.post("/addFood", async(req, res) => {
+        console.log(req.body);
+        const result = await addFoodCollection.insertOne(req.body);
+        console.log(result);
+        res.send(result)
+    })
+    // manage my food page
+    app.get("/myFood/:email", async (req, res) =>{
+        console.log(req.params.email);
+        const result = await addFoodCollection.find({email:req.params.email}).toArray();
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
